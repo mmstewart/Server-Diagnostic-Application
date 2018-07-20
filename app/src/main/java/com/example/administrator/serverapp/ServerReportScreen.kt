790b2a8a4b0046541cbde02ef4b6a9b03579abdd
Marcus <MMSStewart20@gmail.com>
2018-07-20 14:31:51 -0500

    ServerReportScreen Updated
    With a crash when you don't have a URL in the URL line

--- app/src/main/java/com/example/administrator/serverapp/ServerReportScreen.kt
+++ app/src/main/java/com/example/administrator/serverapp/ServerReportScreen.kt
@@ -5,34 +5,143 @@ import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.RelativeLayout
-import android.widget.TextView
+import android.webkit.URLUtil
+import android.widget.*
 import okhttp3.*
-import java.io.IOException
+import org.json.JSONArray
+import org.json.JSONException
+import java.net.MalformedURLException
+import java.net.SocketTimeoutException
+import java.net.URL
+import kotlin.concurrent.thread
 
-open class ServerReportScreen : Fragment() {
+class ServerReportScreen : Fragment() {
 /* Use OkHttp or Retrofit to access the network server, comes out as a JSON text. Test using a button first
 then have a line where the user can enter their ip address and submit using the button. */
+
+    //VIEW
+    private var button : Button? = null
+    private var button2 : Button? = null
+    private var urlInput : TextView? = null
+    private var textResult : TextView? = null
+    private var spin : Spinner? = null
+
+    //OKHTTP
+    private var client : OkHttpClient? = null
+    private var request : Request? = null
+    private var http = OkHttp()
+
+    //JSON RETURN
+    private var json : String? = null
+    private var jsonResp = JSONArray()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view : View = inflater.inflate(R.layout.activity_server_report_screen, container,false)
 
-        val button : Button = view.findViewById(R.id.button1)
-        val button2 : Button = view.findViewById(R.id.button3)
-        val textResult : TextView = view.findViewById(R.id.textView2)
         val rL : RelativeLayout = view.findViewById(R.id.relative_layout)
+        val progressBar : ProgressBar = view.findViewById(R.id.PB)
+        val locationList = ArrayList<Location>()
 
+        urlInput = view.findViewById<View>(R.id.editText) as TextView
+        spin = view.findViewById<View>(R.id.comboBox) as Spinner
+        textResult= view.findViewById(R.id.textView2) as TextView
+        button = view.findViewById(R.id.button1) as Button
+        button2 = view.findViewById(R.id.button3) as Button
+
+        //Buttons are disabled initially
+        button!!.isEnabled = false
+        button2!!.isEnabled = false
+
+        //Once a character is typed, buttons are enabled
+        urlInput!!.addTextChangedListener(object: TextWatcher {
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if(p0.toString().trim() == "") {
+                    button!!.isEnabled = false
+                    button2!!.isEnabled = false
+                } else {
+                    button!!.isEnabled = true
+                    button2!!.isEnabled = true
+                }
+            }
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+            override fun afterTextChanged(p0: Editable?) { }
+        })
+
+        //Sets Home Screen title of action bar to "Server Diagnostic Report"
         (activity as AppCompatActivity).supportActionBar!!.title = "Server Diagnostic Report"
 
+        //Progress Bar/Loading symbol is disabled at default
+        progressBar.visibility = View.GONE
+
         //Submit Button
-        button.setOnClickListener {
-            webService()
+        button!!.setOnClickListener {
+            //Progress Bar is enabled when searching for the URL
+            progressBar.visibility = View.VISIBLE
+
+            textResult!!.text = "Waiting for response..."
+            thread {
+                try {
+                    client = http.client
+                    request = http.getRequest(urlInput!!.text.toString())
+                    json = http.getURL(client, request)
+
+                    //Hides keyboard when the submit button is pressed
+                    val imm : InputMethodManager = activity!!.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(activity!!.currentFocus.windowToken, 0)
+
+                    activity!!.runOnUiThread {
+                        try {
+                            jsonResp = org.json.JSONArray(json)
+                            var s = ""
+                            locationList.clear()
+                            for (i in 0 until jsonResp.length()) {
+                                val r = Location()
+                                s += "Location: " + jsonResp.getJSONObject(i).getString("locationName") + "\n" +
+                                        "Latitude: " + jsonResp.getJSONObject(i).getString("latitude") + "\n" +
+                                        "Longitude: " + jsonResp.getJSONObject(i).getString("longitude") + "\n\n"
+
+                                r.setLocationName(jsonResp.getJSONObject(i).getString("locationName"))
+                                r.setLatitude(jsonResp.getJSONObject(i).getString("latitude"))
+                                r.setLongitude(jsonResp.getJSONObject(i).getString("longitude"))
+
+                                locationList.add(r)
+                            }
+                            val locationString = ArrayList<String>()
+                            for (i in locationList){
+
+                                locationString.add(i.getLocationName().toString())
+                            }
+                            textResult!!.text = s
+
+                            spin!!.adapter = ArrayAdapter<String>(activity, android.R.layout.simple_spinner_item, locationString)
+
+                            spin!!.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                                override fun onNothingSelected (parent: AdapterView<*>?) {
+                                    textResult!!.text = "Select an option"
+                                }
+                                override fun onItemSelected (parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                                    //
+                                }
+                            }
+                        } catch (e : JSONException) {
+                            textResult!!.text = json
+                            //Progress Bar is disabled once the URL is loaded
+                            progressBar.visibility = View.GONE
+                        } catch (e : SocketTimeoutException) {
+                            textResult!!.text = e.message
+                        }
+                    }
+                } catch (e : IllegalArgumentException) {
+                    textResult!!.text = e.message
+                }
+            }
         }
 
         //Hides keyboard on touch outside of EditText
@@ -42,8 +151,9 @@ then have a line where the user can enter their ip address and submit using the 
         }
 
         //Clear Button
-        button2.setOnClickListener {
-            textResult.text = "Results"
+        button2!!.setOnClickListener {
+            textResult!!.text = "Results Appear Here"
+            urlInput!!.text = ""
         }
         return view
     }
@@ -52,38 +162,5 @@ then have a line where the user can enter their ip address and submit using the 
     private fun View.hideKeyboard(view : View) {
         val imm : InputMethodManager = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
-    }
-
-    //Method downloads a URL and prints the JSON file into a string
-    private fun webService() {
-        val textResult : TextView = view!!.findViewById(R.id.textView2)
-        val client = OkHttpClient()
-        var textURL = "https://reqres.in/api/users?page=2" //Feel free to change URL
-
-        //Hides keyboard on submit
-        val i : InputMethodManager = activity!!.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        i.hideSoftInputFromWindow(activity!!.currentFocus.windowToken, 0)
-        
-        //Feel free to edit here
-        val request : Request = Request.Builder()
-                .url(textURL)
-                .build()
-
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                activity!!.runOnUiThread {
-                    textResult.text = "Failure"
-                }
-            }
-            override fun onResponse(call: Call, response: Response) {
-                activity!!.runOnUiThread {
-                    try {
-                        textResult.text = response.body()!!.string()
-                    } catch (e: IOException) {
-                        textResult.text = "Error during get body"
-                    }
-                }
-            }
-        })
     }
 }
