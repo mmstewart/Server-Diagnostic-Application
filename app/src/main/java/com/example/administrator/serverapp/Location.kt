1cf82464864ad9b3d6fd09104da5aa4894a17fd7
Marcus <MMSStewart20@gmail.com>
2018-08-06 16:19:03 -0500

    Removed unnecessary methods

--- app/src/main/java/com/example/administrator/serverapp/Location.kt
+++ app/src/main/java/com/example/administrator/serverapp/Location.kt
@@ -5,32 +5,14 @@ class Location {
     private var longitude: String? = null
     private var latitude: String? = null
 
-    constructor(locationName: String, longitude: String, latitude: String) {
-        this.locationName = locationName
-        this.longitude = longitude
-        this.latitude = latitude
-    }
-
     constructor()
-
-    fun getLocationName(): String? {
-        return this.locationName
-    }
 
     fun setLocationName(s: String) {
         this.locationName = s
     }
 
-    fun getLongitude(): String? {
-        return this.longitude
-    }
-
     fun setLongitude(s: String) {
         this.longitude = s
-    }
-
-    fun getLatitude(): String? {
-        return this.latitude
     }
 
     fun setLatitude(s: String) {
