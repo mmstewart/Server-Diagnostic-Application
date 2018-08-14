ceaedc072a0ae3505fb56400d3eb88a92cd9518c
Marcus <MMSStewart20@gmail.com>
2018-08-14 15:47:54 -0500

    Update OkHttp.java

--- app/src/main/java/com/example/administrator/serverapp/OkHttp.java
+++ app/src/main/java/com/example/administrator/serverapp/OkHttp.java
@@ -3,11 +3,17 @@ package com.example.administrator.serverapp;
 import java.io.IOException;
 import java.net.ConnectException;
 import java.net.SocketTimeoutException;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLHandshakeException;
+import javax.net.ssl.SSLSession;
+
 import okhttp3.Call;
 import okhttp3.Credentials;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.internal.http2.StreamResetException;
 
 public class OkHttp {
 
@@ -15,6 +21,16 @@ public class OkHttp {
 
         String result;
         OkHttpClient client = new OkHttpClient();
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+
+        builder.hostnameVerifier(new HostnameVerifier() {
+            @Override
+            public boolean verify(String s, SSLSession sslSession) {
+                return true;
+            }
+        });
+        client.hostnameVerifier();
+
         Request request = new Request.Builder()
                 .url("https://" + url)
                 .header("Authorization", Credentials.basic(username, password))
@@ -25,8 +41,8 @@ public class OkHttp {
         try {
             Response response = call.execute();
             result = response.body().string();
-        } catch (ConnectException | SocketTimeoutException e) {
-            return "Failed to connect to " + url;
+        } catch (ConnectException | SocketTimeoutException | SSLHandshakeException | StreamResetException e) {
+            return "Failed to connect to https://" + url + ", try again.";
         }
         return result;
     }
